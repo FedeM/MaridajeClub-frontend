ba9392a00af084329f0c70a1c19080bc744b3fb7
Enzo Santilli <enzo135246@gmail.com>
2022-02-16 16:33:40 -0300

    feat: Comment product filters

--- components/views/shopping/productFilter/Desktop/FilterDesktop.jsx
+++ components/views/shopping/productFilter/Desktop/FilterDesktop.jsx
@@ -2,6 +2,11 @@ import style from './Filter.module.css'
 import Image from 'next/image'
 
 const FilterDesktop = ({name}) => {
+    //Función para aplicar los filtros
+    const applyFilter =(e)=>{
+        
+    }
+
     return (
         <div className={style.filter}>
             <div className={style.content_box}>
@@ -11,21 +16,23 @@ const FilterDesktop = ({name}) => {
                 </div>
                 <div className={style.filter_container}>
                     <div className={style.filter_search}>
-                        <Image className='non-select' draggable="false" src="/assets/img/icon/loupe.png" alt="Botón buscar, search button" width={"18"} height={"18"}/>
                         <input type="search" placeholder='Buscar Bebida, bodega, tipo'/>
+                        <div className={style.img}>
+                            <Image className='non-select' draggable="false" src="/assets/img/icon/loupe.png" alt="Botón buscar, search button" width={"18"} height={"18"}/>
+                        </div>
                     </div>
-                    <div className={style.filter_select}>
-                        <select name="filter" id="">
-                            <option value="mv">Más vendidos</option>
-                            <option value="mc">Más caro</option>
-                            <option value="mb">Más barato</option>
+                    {/* <div className={style.filter_select}>
+                        <select name="filter" id="" onChange={(e)=> applyFilter(e)}>
+                            <option value="0">Más vendidos</option>
+                            <option value="1">Más caro</option>
+                            <option value="2">Más barato</option>
                         </select>
                     </div>
-                    <div><p>Filtro 1</p><input type="checkbox" /></div>
-                    <div><p>Filtro 2</p><input type="checkbox" /></div>
-                    <div><p>Filtro 3</p><input type="checkbox" /></div>
-                    <div><p>Filtro 4</p><i className="fas fa-chevron-down"></i></div>
-                    <div><p>Filtro 5</p><i className="fas fa-chevron-down"></i></div>
+                    <div className="style.filterDiv"><p>Filtro 1</p><input type="checkbox" /></div>
+                    <div className="style.filterDiv"><p>Filtro 2</p><input type="checkbox" /></div>
+                    <div className="style.filterDiv"><p>Filtro 3</p><input type="checkbox" /></div>
+                    <div className="style.filterDiv"><p>Filtro 4</p><i className="fas fa-chevron-down"></i></div>
+                    <div className="style.filterDiv"><p>Filtro 5</p><i className="fas fa-chevron-down"></i></div> */}
                 </div>
             </div>
         </div>
--- components/views/shopping/productFilter/Responsive/FilterResponsive.jsx
+++ components/views/shopping/productFilter/Responsive/FilterResponsive.jsx
@@ -26,10 +26,16 @@ const FilterResponsive = () => {
 
     return (
         <>
-            <div className={style.container}>
+            <div className={style.filter_search}>
+                <input type="search" placeholder='Buscar Bebida, bodega, tipo'/>
+                <div className={style.img}>
+                    <Image className='non-select' draggable="false" src="/assets/img/icon/loupe.png" alt="Botón buscar, search button" width={"18"} height={"18"}/>
+                </div>
+            </div>
+            {/* <div className={style.container}>
                 <div className={style.side} onClick={()=> setSortActivate(true)}><Image src="/assets/img/icon/sort.png" alt="icon order, icono ordenar" width={'18px'} height={'18px'}/><span>Ordenar</span></div>
                 <div className={style.side} onClick={()=> setFilterActivate(true)}><Image src="/assets/img/icon/filter.png" alt="icon filter, icono filtrar" width={'18px'} height={'18px'}/><span>Filtrar</span></div>
-            </div>
+            </div> */}
             {sortBox()}
             {filterBox()}
         </>
